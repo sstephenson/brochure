46c2401f2f189d4c0653c81af30257be987d8b5f
Sam Stephenson <sam@37signals.com>
2010-11-20 15:39:09 -0600

    Brochure 0.5.1

--- lib/brochure.rb
+++ lib/brochure.rb
@@ -3,7 +3,7 @@ require "rack"
 require "tilt"
 
 module Brochure
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 
   autoload :Application,         "brochure/application"
   autoload :CaptureNotSupported, "brochure/errors"
